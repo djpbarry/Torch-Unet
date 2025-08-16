88d9731ce73367fc6de8eb665f1ecddc33e3542d
Dave Barry <davejpbarry@gmail.com>
2025-08-16 10:10:14 +0300

    Switch to two-branch model

--- Train_U-Net.py
+++ Train_U-Net.py
@@ -12,7 +12,7 @@ import torchvision.transforms.functional as TF
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 
-from regression_model import *
+from two_branch_regression import *
 
 # from datetime import datetime
 
@@ -332,7 +332,7 @@ if __name__ == "__main__":
     if not (abs(train_ratio + val_ratio) < 1.0):
         print("Warning: Sum of TRAIN_RATIO, VAL_RATIO, TEST_RATIO does not equal 1.0.")
 
-    model = AdvancedRegressionModel(initial_filters=128, num_conv_blocks=6)
+    model = SimplifiedTwoBranchRegressionModel(initial_filters_per_branch=128)
     print(f'Using {ncpus} cpu workers.')
 
     # --- Create a unique output directory for this run ---
@@ -454,7 +454,7 @@ if __name__ == "__main__":
     plt.close()  # Close the plot to free memory
 
     print("\n--- Evaluating Model on Test Set ---")
-    loaded_model = AdvancedRegressionModel(initial_filters=128, num_conv_blocks=6)
+    loaded_model = SimplifiedTwoBranchRegressionModel(initial_filters_per_branch=128)
     loaded_model.load_state_dict(torch.load(model_save_path, map_location=device))
     loaded_model.eval()
     loaded_model.to(device)
